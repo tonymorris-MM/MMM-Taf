5d83094ece1c0d12f39e862cfac6b2ef7e0b7c31
Adam Morris <amorris2003@gmail.com>
2019-10-04 16:23:25 +1000

    Allow font-size

--- MMM-Taf.js
+++ MMM-Taf.js
@@ -17,14 +17,14 @@ Module.register("MMM-Taf", {
     },
     getDom: function() {
         var wrapper = document.createElement('div');
-
+        var self = this;
         this.results.forEach(function(entry) {
             var wrapperDiv = document.createElement('div');
             var wrapperBreak = document.createElement('br');
             entry.forEach(function(entry) {
                 var entryDiv = document.createElement('div');
                 entryDiv.innerHTML = entry;
-                entryDiv.setAttribute('font-size', this.config.fontSize +'px');
+                entryDiv.setAttribute('font-size', self.config.fontSize +'px');
                 wrapperDiv.appendChild(entryDiv);
             });
             wrapper.appendChild(wrapperDiv);
